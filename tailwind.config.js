fc6600b0a68885f818e88207f0b46af9292d781f
Trine Færaas <trine@Trines-MacBook-Pro-2.local>
2021-04-30 10:54:59 +0200

    Use custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@ module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      stifinner: {
+         400: '#fdfdfe', // Ekstra lys stifinnerblå
+         500: '#becad7', // Lys stifinner
+         600: '#596578', // Stifinner
+         700: '#394b5d', // Mørk stifinner
+         800: '#2c3a48', // Ekstra mørk stifinner
+         900: '#192028', // Dypeste stifinner
+      },
+      speider: {
+         400: '#fceb96', // Lys småspeidergul
+         500: '#f2cc07', // Småspeidergul
+      },
+      vandrer: {
+         300: '#f1f9ed', // Lys grønn
+         400: '#d6e6d2', // Lys vandrergrønn
+         500: '#99c49a', // Medium vandrergrønn
+      },
+   }
   },
   variants: {},
   plugins: [],
