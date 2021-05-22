a7e07078ae39f2a37076fe4558d20e99c702f43e
trinef <trine@studionetting.no>
2021-05-22 14:21:27 +0200

    Nav bar styling (#1)
    * Restructure html
    
    * Update favicons
    
    * Add more colors
    
    * Add child pages
    
    * Add submenu items
    
    * Hide menu on tablet
    
    * Grids for breakpoints
    
    * Update tailwind and postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,39 @@ module.exports = {
       transparent: 'transparent',
       current: 'currentColor',
       stifinner: {
-         400: '#fdfdfe', // Ekstra lys stifinnerblå
-         500: '#becad7', // Lys stifinner
-         600: '#596578', // Stifinner
-         700: '#394b5d', // Mørk stifinner
-         800: '#2c3a48', // Ekstra mørk stifinner
-         900: '#192028', // Dypeste stifinner
+         lighter: '#fdfdfe', // Ekstra lys stifinnerblå
+         light: '#becad7', // Lys stifinner
+         profile: '#596578', // Stifinner
+         dark: '#394b5d', // Mørk stifinner
+         darker: '#2c3a48', // Ekstra mørk stifinner
+         darkest: '#192028', // Dypeste stifinner
       },
       speider: {
-         400: '#fceb96', // Lys småspeidergul
-         500: '#f2cc07', // Småspeidergul
+         light: '#fceb96', // Lys småspeidergul
+         profile: '#f2cc07', // Småspeidergul
       },
       vandrer: {
-         300: '#f1f9ed', // Lys grønn
-         400: '#d6e6d2', // Lys vandrergrønn
-         500: '#99c49a', // Medium vandrergrønn
+         lighter: '#f1f9ed', // Lys grønn
+         light: '#d6e6d2', // Lys vandrergrønn
+         dark: '#99c49a', // Medium vandrergrønn
+      },
+      leder: {
+         light: '#B2E3EF', // Medium lederblå
+         profile: '#70B8D2', // Lederblå
+         dark: '#317E9B', // Mørk lederblå
+      },
+      orange: {
+         light: '#ffd9a1', // Lys orange
+      },
+      red: {
+         light: '#EAB1B4', // Lys rød
+      },
+      event: {
+         'course': '#B2E3EF', // Medium lederblå
+         'camp': '#fceb96', // Lys småspeidergul
+         'hike': '#EAB1B4', // Lys rød
+         'activity': '#ffd9a1', // Lys orange
+         'project': '#becad7', // Lys stifinner
       },
    },
    fontFamily : {
