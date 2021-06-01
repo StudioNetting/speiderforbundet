b6f5135b5b8aee91fc63afe6a34f9b1076c0fc01
Trine <trine@studionetting.no>
2021-06-01 15:17:40 +0200

    Update text styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,55 +3,57 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       stifinner: {
-         lighter: '#fdfdfe', // Ekstra lys stifinnerblå
-         light: '#becad7', // Lys stifinner
-         profile: '#596578', // Stifinner
-         dark: '#394b5d', // Mørk stifinner
-         darker: '#2c3a48', // Ekstra mørk stifinner
-         darkest: '#192028', // Dypeste stifinner
+        lighter: "#FDFDFE", // Ekstra lys stifinnerblå
+        light: "#BECAD7", // Lys stifinner
+        profile: "#596578", // Stifinner
+        dark: "#394B5D", // Mørk stifinner
+        darker: "#2C3A48", // Ekstra mørk stifinner
+        darkest: "#192028", // Dypeste stifinner
       },
       speider: {
-         light: '#fceb96', // Lys småspeidergul
-         profile: '#f2cc07', // Småspeidergul
+        light: "#FCEB96", // Lys småspeidergul
+        profile: "#F2CC07", // Småspeidergul
       },
       vandrer: {
-         lighter: '#f1f9ed', // Lys grønn
-         light: '#d6e6d2', // Lys vandrergrønn
-         dark: '#99c49a', // Medium vandrergrønn
+        lighter: "#F1F9ED", // Lys grønn
+        light: "#D6E6D2", // Lys vandrergrønn
+        dark: "#99C49A", // Medium vandrergrønn
       },
       leder: {
-         light: '#B2E3EF', // Medium lederblå
-         profile: '#70B8D2', // Lederblå
-         dark: '#317E9B', // Mørk lederblå
+        light: "#B2E3EF", // Medium lederblå
+        profile: "#70B8D2", // Lederblå
+        dark: "#317E9B", // Mørk lederblå
       },
       orange: {
-         light: '#ffd9a1', // Lys orange
+        light: "#FFD9A1", // Lys orange
       },
       red: {
-         light: '#EAB1B4', // Lys rød
+        light: "#EAB1B4", // Lys rød
       },
-      white: '#FFFFFF',
+      white: "#FFFFFF",
       event: {
-         'course': '#B2E3EF', // Medium lederblå
-         'camp': '#fceb96', // Lys småspeidergul
-         'hike': '#EAB1B4', // Lys rød
-         'activity': '#ffd9a1', // Lys orange
-         'project': '#becad7', // Lys stifinner
+        project: "#B2E3EF", // Medium lederblå
+        camp: "#FCEB96", // Lys småspeidergul
+        activity: "#EAB1B4", // Lys rød
+        course: "#FFD9A1", // Lys orange
+        hike: "#99C49A", // Lys stifinner
       },
-   },
-   fontFamily : {
-      'sans' : ['DIN Pro', 'DIN', 'arial', 'helvetica', 'sans-serif'],
-      'serif': [ 'Georgia', 'serif'],
-   },
-   fontSize: {
-      small: ['14px', '16px'],
-      base: ['18px', '24px'],
-      large: ['24px', '30px'],
-      xlarge: ['36px', '40px'],
-    }
+    },
+    fontFamily: {
+      sans: ["DIN Pro", "DIN", "arial", "helvetica", "sans-serif"],
+      serif: ["Georgia", "serif"],
+    },
+    fontSize: {
+      xsmall: ["14px", "16px"], // Text style: label
+      small: ["14px", "18px"], // Text style: paragraph sans, menu items
+      basealt: ["18px", "20px"], // Text style: h3
+      base: ["18px", "24px"], // Text style: paragraph serif
+      medium: ["24px", "30px"], // Text style: h2, preamble
+      large: ["36px", "40px"], // Text style: h1
+    },
   },
   variants: {},
   plugins: [],
